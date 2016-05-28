8a56fbbe5e8cfe5b35a5a724b28d090cdce23911
Anfa Abukar <developer@anfa.io>
2016-05-27 20:32:11 -0400

    removed global package wrapper

--- spot.js
+++ spot.js
@@ -1,43 +1,42 @@
-exports.spot = function() {
-    var util = require('util');
-    var exec = require('child_process').exec;
+  var util = require('util');
+  var exec = require('child_process').exec;
 
-    process.chdir(__dirname);
+  process.chdir(__dirname);
 
-    function buildCmd() {
-        process.argv.indexOf('spot');
+  function buildCmd() {
+      console.log('test');
+      process.argv.indexOf('spot');
 
-        if (process.argv.indexOf('play')) {
-            var preBuilt = '';
-            for (var i = process.argv.indexOf('play'); i < process.argv.length; i++) {
-                preBuilt = preBuilt + process.argv[i] + ' ';
-                if (i === process.argv.length - 1) {
-                    console.log(preBuilt);
-                    factoryCmd(preBuilt);
-                }
-            }
-        } else {
-            factoryCmd('');
-        }
+      if (process.argv.indexOf('play')) {
+          var preBuilt = '';
+          for (var i = process.argv.indexOf('play'); i < process.argv.length; i++) {
+              preBuilt = preBuilt + process.argv[i] + ' ';
+              if (i === process.argv.length - 1) {
+                  console.log(preBuilt);
+                  factoryCmd(preBuilt);
+              }
+          }
+      } else {
+          factoryCmd('');
+      }
 
-    }
+  }
 
-    buildCmd();
+  buildCmd();
 
-    function factoryCmd(parts) {
-        var tools = './sp ' + parts;
-        execCmd(tools);
-    }
+  function factoryCmd(parts) {
+      var tools = './sp ' + parts;
+      execCmd(tools);
+  }
 
-    function execCmd(built) {
-        child = exec(built, function(error, childOut, childErr) {
+  function execCmd(built) {
+      child = exec(built, function(error, childOut, childErr) {
 
-            if (!error) {
-                console.log(childOut);
-                console.log(childErr);
-            } else {
-                console.log('there was an error ' + error);
-            }
-        });
-    }
-};
+          if (!error) {
+              console.log(childOut);
+              console.log(childErr);
+          } else {
+              console.log('there was an error ' + error);
+          }
+      });
+  }
